50aa22146222d791d9b0f8b3b07465f6ad1b6f54
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-11-24 11:41:29 -0300

    feat(produto): Criação da classe produto

---
+++ src/main/java/com/matheuspq/domain/Produto.java
@@ -0,0 +1,84 @@
+package com.matheuspq.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String nome;
+	private String categoria;
+	@Column(nullable = false)
+	private BigDecimal valor;
+	
+	public Produto() {
+		
+	}
+
+	public Produto(Long id, String nome, String categoria, BigDecimal valor) {
+		this.id = id;
+		this.nome = nome;
+		this.categoria = categoria;
+		this.valor = valor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+}
