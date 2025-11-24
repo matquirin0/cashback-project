8e0af0da094e485b95bbc502245e502c5142b9f6
Matheus Quirino <contato.matheuspquirino@gmail.com>
2025-11-24 10:44:47 -0300

    Criação do projeto de Cashback

---
+++ src/main/java/com/matheuspq/cashbackProject/CashbackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.matheuspq.cashbackProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashbackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashbackProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuspq/cashbackProject/CashbackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuspq.cashbackProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashbackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
